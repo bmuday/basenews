df04ce76d2689515f91b33e70e7541037a5c30c0
bmuday <bmuday@live.fr>
2023-07-19 11:11:06 +0200

    deploy grafbase api to cloud

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,59 @@
+import { g, auth, config } from "@grafbase/sdk";
+
+// Welcome to Grafbase!
+// Define your data models, integrate auth, permission rules, custom resolvers, search, and more with Grafbase.
+// Integrate Auth
+// https://grafbase.com/docs/auth
+//
+// const authProvider = auth.OpenIDConnect({
+//   issuer: process.env.ISSUER_URL ?? ''
+// })
+//
+// Define Data Models
+// https://grafbase.com/docs/database
+
+const post = g.model("Post", {
+  title: g.string(),
+  url: g.url().unique(),
+  comments: g
+    .relation(() => comment)
+    .optional()
+    .list()
+    .optional(),
+  // slug: g.string().unique(),
+  // content: g.string().optional(),
+  // publishedAt: g.datetime().optional(),
+  // likes: g.int().default(0),
+  // tags: g.string().optional().list().length({ max: 5 }),
+  // author: g.relation(() => user).optional()
+});
+
+const comment = g.model("Comment", {
+  message: g.string(),
+  post: g.relation(post),
+  likes: g.int().default(0),
+  author: g.relation(() => user).optional(),
+});
+
+const user = g.model("User", {
+  name: g.string(),
+  email: g.email().optional(),
+  posts: g.relation(post).optional().list(),
+  comments: g.relation(comment).optional().list(),
+
+  // Extend models with resolvers
+  // https://grafbase.com/docs/edge-gateway/resolvers
+  // gravatar: g.url().resolver('user/gravatar')
+});
+
+export default config({
+  schema: g,
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
+});
